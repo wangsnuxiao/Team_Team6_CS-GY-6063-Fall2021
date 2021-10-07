296d62cfc40c8034e07c2e7437e231d7aa30ea20
Samuel Ballan <sgb4622@gmail.com>
2021-10-07 19:05:05 -0400

    client source

--- server/urls.py
+++ server/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('index.html', 'static/index.html'),
     path('venues/', include('server.venues.urls')),
     path('admin/', admin.site.urls),
 ]
